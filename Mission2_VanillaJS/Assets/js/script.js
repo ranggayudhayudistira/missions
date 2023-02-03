7b5cdc621f1fde686d1b30d1a7bc1e8be2a1970a
greckminas <ranggayudhayudistira79@gmail.com>
2023-02-03 17:34:28 +0700

    Update script.js

--- Mission2_VanillaJS/Assets/js/script.js
+++ Mission2_VanillaJS/Assets/js/script.js
@@ -166,7 +166,7 @@ function randInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 // https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
-function sleep(ms) {
+async function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 // https://javascript.info/js-animation#back-bow-shooting
